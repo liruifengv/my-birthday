0235919a20f328fefe2a24b4179867afdbd175f7
liruifeng <liruifeng@troila.com>
2024-01-23 16:44:32 +0800

    chore: init project

---
+++ packages/my-birthday/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig, Plugin } from "vite";
+import dts from "vite-plugin-dts";
+
+const name = "my-birthday";
+
+export default defineConfig(() => {
+  return {
+    build: {
+      lib: {
+        entry: "index.ts",
+        name: name,
+        fileName: (format) => (format === "es" ? `${name}.mjs` : `${name}.js`),
+      },
+    },
+    plugins: [
+      dts({
+        outDir: "dist/types",
+      }) as unknown as Plugin,
+    ],
+  };
+});
